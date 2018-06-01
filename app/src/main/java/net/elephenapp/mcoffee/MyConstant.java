f807968baa8c6fc19a610f72938ff348b0773e66
Bowon <bowonmai@yahoo.com>
2018-06-01 08:42:16 +0700

    Start 1 Jun

---
+++ app/src/main/java/net/elephenapp/mcoffee/MyConstant.java
@@ -0,0 +1,20 @@
+package net.elephenapp.mcoffee;
+
+public class MyConstant {
+
+//    URL
+    private String urlGetAllUser ="http://infobizplus.com/R2P/php/getAllUser.php";
+    private String  urlGetUserWhwerWserAnPass = "http://infobizplus.com/R2P/php/getValueLogin.php";
+
+
+//    Getter
+
+
+    public String getUrlGetUserWhwerWserAnPass() {
+        return urlGetUserWhwerWserAnPass;
+    }
+
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
+}
